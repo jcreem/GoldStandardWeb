41f0092d4b8b77005b2947b5f3db1bd938a2d529
me@mail.com <me@mail.com>
2016-04-02 21:35:01 -0400

    minor refactoring

--- generator/management/commands/generate_active.py
+++ generator/management/commands/generate_active.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
         os.chdir('/tmp')
 
         for Active in ActiveGoldStandard.objects.all():
-            GS_Filename_Date=Active.goldstandard.session_date.strftime('%m-%d-%y')
+
             GS_Title_Date=Active.goldstandard.session_date.strftime('%A, %B %-d, %Y')
             GS_House=Active.goldstandard.get_gen_court_house_display()
 
@@ -21,8 +21,7 @@ class Command(BaseCommand):
                 Sheet_URL=Active.goldstandard.google_sheet_url,
                 GS_Title= GS_House +
                 ' SESSION - ' + GS_Title_Date,
-                Filename='/tmp/goldstandard-'+ GS_Filename_Date + '-' +
-                 GS_House[0] + '.pdf',
+                Filename=Active.goldstandard.get_pdf_name(Gold_Background = False),
                 JSON_Key_File='/home/jcreem/nhla/gs_tools/NHLAGS-e8b3911072d5.json',
                 Background_Color=generate.White,
                 )
@@ -31,8 +30,7 @@ class Command(BaseCommand):
                 Sheet_URL=Active.goldstandard.google_sheet_url,
                 GS_Title= GS_House +
                 ' SESSION - ' + GS_Title_Date,
-                Filename='/tmp/goldstandard-'+ GS_Filename_Date + '-' +
-                 GS_House[0] + '-y.pdf',
+                Filename=Active.goldstandard.get_pdf_name(Gold_Background = True),
                 JSON_Key_File='/home/jcreem/nhla/gs_tools/NHLAGS-e8b3911072d5.json',
                 Background_Color=generate.Gold,
                 )
--- generator/models.py
+++ generator/models.py
@@ -16,6 +16,17 @@ class GoldStandard(models.Model):
 
         return u'%s - %s' % (self.get_gen_court_house_display(), self.session_date.strftime('%B %d, %Y'))
 
+    def get_pdf_name(self, Gold_Background = True):
+        GS_Filename_Date=Aself.session_date.strftime('%m-%d-%y')
+        GS_House=self.get_gen_court_house_display()
+        if Gold_Background:
+            suffix='-y'
+        else:
+            suffix=''
+        Filename='goldstandard-'+ GS_Filename_Date + '-' +
+                 GS_House[0] + suffix + '.pdf',
+
+
     class Meta:
         unique_together = ('session_date', 'gen_court_house',)
 

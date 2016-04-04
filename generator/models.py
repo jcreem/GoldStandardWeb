4fc44d18050665da760af1662ca96f131a29009a
me@mail.com <me@mail.com>
2016-04-03 22:10:02 -0400

    dos2unix and updates to start to support non-admin access

--- generator/models.py
+++ generator/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+import django_settings
+
+class Text(django_settings.db.Model):
+    value=models.TextField()
+    class Meta:
+        abstract= True
+
 
 class GoldStandard(models.Model):
     HOUSE='Hou'
@@ -17,21 +24,28 @@ class GoldStandard(models.Model):
         return u'%s - %s' % (self.get_gen_court_house_display(), self.session_date.strftime('%B %d, %Y'))
 
     def get_pdf_name(self, Gold_Background = True):
-        GS_Filename_Date=Aself.session_date.strftime('%m-%d-%y')
-        GS_House=self.get_gen_court_house_display()
+        GS_Filename_Date=self.session_date.strftime('%m-%d-%y')
+
+        GS_House=str(self.get_gen_court_house_display())
         if Gold_Background:
             suffix='-y'
         else:
             suffix=''
-        Filename='goldstandard-'+ GS_Filename_Date + '-' +
-                 GS_House[0] + suffix + '.pdf',
 
+        Filename='goldstandard-'
+        Filename=Filename + GS_Filename_Date + '-' + \
+          GS_House[0] + suffix + '.pdf'
+        return Filename
 
     class Meta:
         unique_together = ('session_date', 'gen_court_house',)
+
+
 
 class ActiveGoldStandard(models.Model):
     goldstandard = models.OneToOneField(GoldStandard, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.goldstandard.__unicode__()
+
+django_settings.register(Text)

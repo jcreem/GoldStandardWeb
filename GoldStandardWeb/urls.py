645190dfa28552a540b122e10e0601ee5b0ee4b8
Jeffrey Creem <jeff@thecreems.com>
2016-04-29 21:42:59 -0400

    Map the / URL to the generator app via a response redirect

--- GoldStandardWeb/urls.py
+++ GoldStandardWeb/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.http import HttpResponseRedirect
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('generator/')),
     url(r'^polls/', include('polls.urls')),
     url(r'^generator/', include('generator.urls')),
     url(r'^admin/', admin.site.urls),
